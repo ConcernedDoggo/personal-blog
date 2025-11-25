8ccafd9567858a9a739c467d4b1ece095a6a7551
Anil Chavan <harshal.anil-chavan@capgemini.com>
2025-11-25 11:11:26 +0530

    add metadata for SEO on blog pages

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,18 +1,40 @@
+import { metadata } from "@/app/layout";
 import { getArticleData } from "@/lib/articles"
+import type { Metadata } from "next";
 
-const Article = async ({params}: {params: {id: string}}) => {
-    const { id }  = await params;
+
+
+
+export async function generateMetadata({params}: {params: {id: string}}): Promise<Metadata> {
+    const { id } = await params;
     const articleData = await getArticleData(id);
 
+  return {
+    title: articleData.title,
+    description: `Blog on ${articleData.title}`,
+    keywords: articleData.category || "blog, article",
+    openGraph: {
+      title: articleData.title,
+      description: `Blog on ${articleData.title}`,
+      type: "article",
+    },
+  };
+}
+
+
+const Article = async ({params}: {params: {id: string}}) => {
+
+    const { id } = await params;
+    const articleData = await getArticleData(id);
+    
     return (
         <section className="mx-auto w-10/12 md:w-1/2 mt-20 flex flex-col gap-5">
             <div className="flex justify-between ">
                 <p>{articleData.date.toString()}</p>
             </div>
-            <article className="article" dangerouslySetInnerHTML={{__html: articleData.contentHTML}} />
+            <article className="article" dangerouslySetInnerHTML={{ __html: articleData.contentHTML }} />
         </section>
     )
 }
-
 
 export default Article;
